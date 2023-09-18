a2e4dcac7a0d9439c2e3888b26e3d0250f1fc6f5
ksharonin <ksharonin@berkeley.edu>
2023-09-18 02:23:34 +0000

    small edits to VEDA API reading

--- .ipynb_checkpoints/Input_VEDA-checkpoint.py
+++ .ipynb_checkpoints/Input_VEDA-checkpoint.py
@@ -4,15 +4,19 @@ Input_VEDA Class
 """
 
 # import statements
+from owslib.ogcapi.features import Features
+import geopandas as gpd
+import datetime as dt
+from datetime import datetime, timedelta
 
 class InputVEDA():
     """ InputVEDA
         Object representing VEDA 
         E.g. 
-    
     """
     
     # global variables here
+    FIRE_OGC_URL = "https://firenrt.delta-backend.com"
     
     # instance initiation
     def __init__(self, crs, title, access_type, ):
@@ -20,6 +24,11 @@ class InputVEDA():
         self.title = title
         self.access_type = access_type
          
-            
+          
+    # attribute access
     def get_crs(self):
         return self.crs
+    
+    def read_API(self):
+        """ API Read """
+        return 0
