13e8c11c41e05d1497ac45a02dc0238114cfccc9
ksharonin <ksharonin@berkeley.edu>
2023-09-18 20:00:40 +0000

    veda input dev

--- Input_VEDA.py
+++ Input_VEDA.py
@@ -4,6 +4,12 @@ Input_VEDA Class
 """
 
 # import statements
+
+import glob
+import logging
+import pandas as pd
+import geopandas as gpd
+from pyproj import CRS
 from owslib.ogcapi.features import Features
 import geopandas as gpd
 import datetime as dt
@@ -12,23 +18,54 @@ from datetime import datetime, timedelta
 class InputVEDA():
     """ InputVEDA
         Object representing VEDA 
-        E.g. 
+        E.g. firenrt dataset 
+        https://nasa-impact.github.io/veda-docs/notebooks/tutorials/mapping-fires.html
     """
     
-    # global variables here
-    FIRE_OGC_URL = "https://firenrt.delta-backend.com"
+    # global
+    TITLE_SETS = ["firenrt", "staging-stac", "staging-raster"]
+
+    OGC_URLS = { "firenrt":  "https://firenrt.delta-backend.com",
+                "staging-stac": "https://staging-stac.delta-backend.com",
+                "staging-raster" : "https://staging-raster.delta-backend.com" }
     
     # instance initiation
-    def __init__(self, crs, title, access_type, ):
-        self.crs = crs
+    def __init__(self, title: str, 
+                 collection: str, 
+                 access_type="api": str, 
+                 crs: str, ):
+        
+        # coordinate ref system
+        self.crs = CRS.from_user_input(int(crs))
+        # extract from crs
+        self.units = self.crs.axis_info[0].unit_name
+        # name for set - user input from choice
         self.title = title
+        # set null till time 
+        self.api_url = None
+        # platform type
         self.access_type = access_type
-         
-          
-    # attribute access
+        
+        if access_type == "api":
+            assert title in InputVEDA.TITLE_SETS, "ERR INPUTVEDA: Invalid title provided"
+            self.set_api_url()
+            self.set_api_collection()
+        else:
+            logging.warning('API NOT SELECTED: discretion advised due to direct file access.')
+                                            
+        
     def get_crs(self):
         return self.crs
+                                            
+    def set_api_url(self):
+        """ fetch api url based on valid title"""
+        if self.title in InputVEDA.OGC_URLS:
+            self.api_url = OGC_URLS[title]
     
-    def read_API(self):
-        """ API Read """
-        return 0
+    def set_api_collection(self):
+        """ set collection with title"""
+        assert self.title is not None, "ERR INPUTVEDA: cannot fetch using null based title"
+        
+                
+    def fetch_collection(self, collec_name: str):
+        
