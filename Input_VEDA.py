3d8fc57b10b8df2d07f9ce8549195f5d786424a3
ksharonin <ksharonin@berkeley.edu>
2023-09-18 22:35:44 +0000

    grow VEDA with scratchwork

--- Input_VEDA.py
+++ Input_VEDA.py
@@ -33,39 +33,59 @@ class InputVEDA():
     def __init__(self, title: str, 
                  collection: str, 
                  access_type="api": str, 
-                 crs: str, ):
-        
-        # coordinate ref system
-        self.crs = CRS.from_user_input(int(crs))
+                 ):
+  
         # extract from crs
         self.units = self.crs.axis_info[0].unit_name
         # name for set - user input from choice
         self.title = title
-        # set null till time 
-        self.api_url = None
+        # collection name
+        self.collection = collection
         # platform type
         self.access_type = access_type
+        
+        self.api_url = None
+        self.bbox = None
+        self.crs = None
+        self.start = None
+        self.stop = None
+        
         
         if access_type == "api":
             assert title in InputVEDA.TITLE_SETS, "ERR INPUTVEDA: Invalid title provided"
             self.set_api_url()
-            self.set_api_collection()
+            self.fetch_api_collection()
         else:
             logging.warning('API NOT SELECTED: discretion advised due to direct file access.')
-                                            
-        
-    def get_crs(self):
-        return self.crs
-                                            
+    
+    # API DATA ACCESS
+    @classmethod
     def set_api_url(self):
         """ fetch api url based on valid title"""
         if self.title in InputVEDA.OGC_URLS:
             self.api_url = OGC_URLS[title]
-    
-    def set_api_collection(self):
-        """ set collection with title"""
-        assert self.title is not None, "ERR INPUTVEDA: cannot fetch using null based title"
+            
+    @classmethod           
+    def fetch_api_collection(self) -> dict:
+        """ return collection using url + set up instance attributes"""
         
-                
-    def fetch_collection(self, collec_name: str):
+        assert self.api_url is not None, "ERR INPUTVEDA: cannot fetch with a null API url"
+        
+        # read out
+        get_collections = Features(url=self.api_url).feature_collections()
+        perm = get_collections.collection(self.collection)
+        
+        # set extent info with properties 
+        self.bbox = perm["extent"]["spatial"]["bbox"]
+        self.crs = perm["extent"]["spatial"]["crs"]
+        self.start = perm["extent"]["temporal"]["interval"][0][0]
+        self.stop = perm["extent"]["temporal"]["interval"][0][1]
+        
+        return perm
+    
+    # HARDCODED DATA ACCESS
+    @classmethod
+    def read_hard_dataset(self):
+        """ read data from passed url/location"""
+        self.crs = CRS.from_user_input(int(crs))
         
